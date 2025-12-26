416a663c2deccc845d930fe100fef10d0e228cd9
Nick Beuchat <nick.beuchat@gmail.com>
2025-12-26 16:28:49 -0500

    another fix

--- src/components/ui/RSVPForm.js
+++ src/components/ui/RSVPForm.js
@@ -64,7 +64,7 @@ export default function RSVPForm() {
     <form
       name="rsvp"
       method="POST"
-      netlify
+      netlify="true"
       onSubmit={handleSubmit}
       className={styles.form}
     >
